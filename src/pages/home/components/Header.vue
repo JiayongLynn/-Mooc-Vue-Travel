e4e0db43485b6c87e3ccb05c539943648b2ea860
510606414@qq.com <510606414@qq.com>
2019-07-22 00:11:13 +0800

    header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="header">
+      <div class="header-left">
+        <div class="iconfont back-icon">&#xe624;</div>
+      </div>
+      <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题
+      </div>
+      <div class="header-right">
+        <span style="font-size:.24rem">城市</span>
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  .header
+    line-height: .86rem
+    display: flex
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align:center
+        font-size:.4rem
+    .header-input
+      flex:  1
+      background :#fff
+      height :.62rem
+      line-height .62rem
+      color:#ccc
+      border-radius: .1rem
+      margin-top :  .12rem
+      margin-left :.2rem
+      padding-left :.2rem
+    .header-right
+      width:  1.24rem
+      text-align: center
+      font-size :0
+      .arrow-icon
+        font-size: 0.24rem
+        // margin-left :-.04rem
+</style>
