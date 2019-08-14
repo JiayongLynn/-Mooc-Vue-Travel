0751acfcd99b0d090e7c825ecb4a9b02c8a382cc
510606414@qq.com <510606414@qq.com>
2019-08-15 00:14:04 +0800

    index-like init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
       <home-swiper></home-swiper>
       <home-icons></home-icons>
       <home-recommend></home-recommend>
+      <home-like></home-like>
     </div>
 </template>
 
@@ -12,13 +13,15 @@ import HomeHeader    from './components/Header'
 import HomeSwiper    from './components/Swiper'
 import HomeIcons     from './components/Icons'
 import HomeRecommend from './components/Recommend'
+import HomeLike      from './components/Like'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend
+    HomeRecommend,
+    HomeLike
   }
 }
 </script>
---
+++ src/pages/home/components/Like.vue
@@ -0,0 +1,36 @@
+<template>
+      <div class="index-like">
+          <div class="like-title">
+              <span class="iconfont icon-like">&#xe85c;</span>
+              <span class="like-left-title">猜你喜欢</span>
+          </div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeLike',
+   data () {
+     return{
+
+     }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+
+    .index-like
+      background-color #ffffff
+      margin-top .2rem
+      .like-title
+        padding:.24rem
+        height .44rem
+        position relative
+        .icon-like
+          color #d81e06
+        ..like-left-title
+          font-size: .32rem
+          line-height .44rem
+</style>
