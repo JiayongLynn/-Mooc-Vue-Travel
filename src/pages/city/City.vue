8a42c911616610d999c1432375cb765d3ac4fceb
JiayongLynn <510606414@qq.com>
2019-08-17 17:58:22 +0800

    city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,45 +1,35 @@
 <template>
-    <div class="header">
-      <div class="header-left">
+    <div class="city">
+      <div class="city-left">
         <div class="iconfont back-icon">&#xe624;</div>
       </div>
-      <div class="header-input">
-        <span class="iconfont">&#xe632;</span>
-        输入城市/景点/游玩主题
+      <div class="city-title">
+        城市选择
       </div>
-      <router-link to="/city">
-        <div class="header-right">
-          <span style="font-size:.24rem">{{this.city}}</span>
-          <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
-      </router-link>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeHeader',
-  props:{
-    city:{
-      type:String
-    }
-  }
+  name: 'City',
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl';
-  .header
+  .city
     line-height: .86rem
     display: flex
     background: $bgColor
     color: #fff
-    .header-left
+    .city-left
       width: .64rem
       float: left
       .back-icon
         text-align:center
-        font-size:.4rem
+        font-size:.36rem
+        width .8rem
+        height .88rem
     .header-input
       flex:  1
       background :#fff
