8e9297640949477ee5c794f2fc9a278527f8059d
JiayongLynn <510606414@qq.com>
2019-08-19 15:31:47 +0800

    city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,51 +1,23 @@
 <template>
-    <div class="city">
-      <div class="city-left">
-        <div class="iconfont back-icon">&#xe624;</div>
-      </div>
-      <div class="city-title">
-        城市选择
-      </div>
-    </div>
+  <div class="city">
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
+ 
 </template>
 
 <script>
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
   name: 'City',
+  components: {
+    CityHeader,
+    CitySearch
+  },
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import '~styles/varibles.styl';
-  .city
-    line-height: .86rem
-    display: flex
-    background: $bgColor
-    color: #fff
-    .city-left
-      width: .64rem
-      float: left
-      .back-icon
-        text-align:center
-        font-size:.36rem
-        width .8rem
-        height .88rem
-    .header-input
-      flex:  1
-      background :#fff
-      height :.62rem
-      line-height .62rem
-      color:#ccc
-      border-radius: .1rem
-      margin-top :  .12rem
-      margin-left :.2rem
-      padding-left :.2rem
-    .header-right
-      width:  1.24rem
-      text-align: center
-      font-size :0
-      color #fff
-      .arrow-icon
-        font-size: 0.24rem
-        // margin-left :-.04rem
+
 </style>
---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+   <div class="city-header">
+      <div class="city-left">
+        <div class="iconfont back-icon">&#xe624;</div>
+      </div>
+        <h2 class="city-title">城市选择</h2>      
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  .city-header
+    background: $bgColor
+    color: #fff
+    height .86rem
+    .city-left
+      width: .64rem
+      position absolute
+      left 0
+      display inline-block
+      .back-icon
+        text-align:center
+        font-size:.36rem
+        width .8rem
+        height .86rem
+        line-height .86rem
+    .city-title
+      font-size:.32rem
+      text-align center
+      line-height .86rem
+  .city:after
+      content: " ";   
+      border-bottom:1px solid #e0e0e0;
+      transform: scaleY(.33333);
+      bottom:0;
+      position absolute
+      width 100%
+</style>
